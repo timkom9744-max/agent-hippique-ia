62266177787d3a4dee6647c254db1bbbf96e6b1c
timkom9744-max <timkom9744@gmail.com>
2026-07-19 21:56:09 +0200

    lecture_html.py

--- lecture_html.py
+++ lecture_html.py
@@ -1,10 +1,30 @@
 from bs4 import BeautifulSoup
 
 
-def lire_titre(html):
+def lire_infos(html):
     soup = BeautifulSoup(html, "html.parser")
 
-    if soup.title:
-        return soup.title.text.strip()
+    texte = soup.get_text(separator=" ", strip=True)
 
-    return "Titre introuvable"
+    mots = [
+        "R1",
+        "C1",
+        "C2",
+        "C3",
+        "C4",
+        "C5",
+        "C6",
+        "C7",
+        "C8",
+        "Prix",
+        "Départ",
+        "Partants",
+    ]
+
+    trouves = []
+
+    for mot in mots:
+        if mot in texte:
+            trouves.append(mot)
+
+    return trouves
