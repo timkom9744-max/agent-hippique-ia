865df28be533c3bdc8c84796f2a46c3549c5be9f
timkom9744-max <timkom9744@gmail.com>
2026-07-19 19:58:12 +0200

    course.py

---
+++ course.py
@@ -0,0 +1,23 @@
+class Course:
+
+    def _init_(self):
+        self.reunion = ""
+        self.course = ""
+        self.hippodrome = ""
+        self.heure = ""
+        self.distance = ""
+        self.discipline = ""
+        self.allocation = ""
+        self.partants = []
+
+    def afficher(self):
+        return (
+            f"Réunion : {self.reunion}\n"
+            f"Course : {self.course}\n"
+            f"Hippodrome : {self.hippodrome}\n"
+            f"Heure : {self.heure}\n"
+            f"Distance : {self.distance}\n"
+            f"Discipline : {self.discipline}\n"
+            f"Allocation : {self.allocation}\n"
+            f"Nombre de partants : {len(self.partants)}"
+        )
